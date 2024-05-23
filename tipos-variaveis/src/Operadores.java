d813f6279d02be53d783069583c4421d91fb5868
launa3 <luanasilvaoli123@gmail.com>
2024-05-23 08:56:14 -0300

    estudos sobre os operadores JAVA

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,5 +1,16 @@
 public class Operadores {
     public static void main(String[] args) {
 
+        /* Forma de mais abreviada */
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = (a == b) ? "verdadeiro" : "false";
+
+        System.out.println(resultado);
+
     }
 }
